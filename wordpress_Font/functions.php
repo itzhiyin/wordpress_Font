490f4b1fdedf7904b13a78ffc18b0e02cc48a6c1
宁子 <95423120+itzhiyin@users.noreply.github.com>
2023-11-26 13:31:47 +0800

    Add files via upload

---
+++ wordpress_Font/functions.php
@@ -0,0 +1,110 @@
+<?php
+if (!defined('ABSPATH')) {
+    die('-1');
+}
+
+function wordpress_Font_hook_ysbth_css() {
+?>
+<style>
+/*字体-start*/
+/*Wordpress替换前端字体插件生成:https://www.itzhiyin.cn/431.html*/
+@font-face{
+  font-family:"itzhiyin";
+  src:url(/wp-content/plugins/wordpress_Font/font/ysbth.woff);
+}
+*{font-family:"itzhiyin"}
+/*Wordpress替换前端字体插件生成:https://www.itzhiyin.cn/431.html*/
+/*字体-end*/
+</style>
+
+<?php } 
+function wordpress_Font_hook_wdch_css() {
+?>
+<style>
+/*字体-start*/
+/*Wordpress替换前端字体插件生成:https://www.itzhiyin.cn/431.html*/
+@font-face{
+  font-family:"itzhiyin";
+  src:url(/wp-content/plugins/wordpress_Font/font/wdch.ttf);
+}
+*{font-family:"itzhiyin"}
+/*Wordpress替换前端字体插件生成:https://www.itzhiyin.cn/431.html*/
+/*字体-end*/
+</style>
+
+<?php }
+function wordpress_Font_hook_ceyyt_css() {
+?>
+<style>
+/*字体-start*/
+/*Wordpress替换前端字体插件生成:https://www.itzhiyin.cn/431.html*/
+@font-face{
+  font-family:"itzhiyin";
+  src:url(/wp-content/plugins/wordpress_Font/font/ceyyt.ttf);
+}
+*{font-family:"itzhiyin"}
+/*Wordpress替换前端字体插件生成:https://www.itzhiyin.cn/431.html*/
+/*字体-end*/
+</style>
+
+<?php }
+function wordpress_Font_hook_muyao_css() {
+?>
+<style>
+/*字体-start*/
+/*Wordpress替换前端字体插件生成:https://www.itzhiyin.cn/431.html*/
+@font-face{
+  font-family:"itzhiyin";
+  src:url(/wp-content/plugins/wordpress_Font/font/muyao.ttf);
+}
+*{font-family:"itzhiyin"}
+/*Wordpress替换前端字体插件生成:https://www.itzhiyin.cn/431.html*/
+/*字体-end*/
+</style>
+
+<?php }
+function wordpress_Font_hook_yrdzst_css() {
+?>
+<style>
+/*字体-start*/
+/*Wordpress替换前端字体插件生成:https://www.itzhiyin.cn/431.html*/
+@font-face{
+  font-family:"itzhiyin";
+  src:url(/wp-content/plugins/wordpress_Font/font/yrdzst.ttf);
+}
+*{font-family:"itzhiyin"}
+/*Wordpress替换前端字体插件生成:https://www.itzhiyin.cn/431.html*/
+/*字体-end*/
+</style>
+
+<?php }
+function wordpress_Font_hook_jcyt_css() {
+?>
+<style>
+/*字体-start*/
+/*Wordpress替换前端字体插件生成:https://www.itzhiyin.cn/431.html*/
+@font-face{
+  font-family:"itzhiyin";
+  src:url(/wp-content/plugins/wordpress_Font/font/jcyt.ttf);
+}
+*{font-family:"itzhiyin"}
+/*Wordpress替换前端字体插件生成:https://www.itzhiyin.cn/431.html*/
+/*字体-end*/
+</style>
+
+<?php }
+function wordpress_Font_hook_zyhl_css() {
+?>
+<style>
+/*字体-start*/
+/*Wordpress替换前端字体插件生成:https://www.itzhiyin.cn/431.html*/
+@font-face{
+  font-family:"itzhiyin";
+  src:url(/wp-content/plugins/wordpress_Font/font/zyhl.otf);
+}
+*{font-family:"itzhiyin"}
+/*Wordpress替换前端字体插件生成:https://www.itzhiyin.cn/431.html*/
+/*字体-end*/
+</style>
+
+<?php }
